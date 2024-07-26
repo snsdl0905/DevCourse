6fa0ec6c4d2061bd6646ab5310a57378353bab0a
Euiyeong <105384780+snsdl0905@users.noreply.github.com>
2024-07-26 22:47:55 +0900

    feat: 객체

---
+++ 01_JS/6-3-4.js
@@ -0,0 +1,16 @@
+// const 물건1 = ['우유', '식빵'];
+// const 물건2 = 물건1;
+// 물건2.push('고구마');
+// 물건2.push('토마토');
+
+// console.log(물건1)
+// console.log(물건2)
+
+const 물건1 = ['우유', '식빵'];
+const 물건2 =[...물건1];
+물건2.push('고구마');
+물건2.push('토마토');
+
+console.log(물건1)
+console.log(물건2)
+
